f01403abe40ddae1fd6d17b853a6b0378c14a7e1
tomaboro <funnymotek@gmail.com>
2017-05-30 20:14:54 +0200

    exercise 2 done

--- generate_str.c
+++ generate_str.c
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------
-// Kolos 2015/16 CZW 11:00
+// Kolos 2015/16 CZW 12:30
 //
 // To compile&link: gcc -o generate_str generate_str.c generate_str.s
 // To run:          ./generate_str
@@ -7,14 +7,17 @@
 //---------------------------------------------------------------
 
 #include <stdio.h>
+#include <stdlib.h>
 
 char* generate_str( char* s,int c,int n, int inc);
 
 int main( void )
 {
-	char* str = malloc(100);
+	char *str = malloc(100);
 	
 	//Running few tests
-	printf("%s\n",generate_str(str,1,1,1);
+	printf("generate_str(str,'f',9,0) = %s\n",generate_str(str,'f',9,0));
+	printf("generate_str(str,'a',7,0) = %s\n",generate_str(str,'a',7,0));
+	printf("generate_str(str,'b',3,1) = %s\n",generate_str(str,'b',3,1));
 	return 0;
 }
--- generate_str.s
+++ generate_str.s
@@ -5,18 +5,28 @@ napis:
 #miejsce na kod
 .text
 .type generate_str, @function #deklaracja funkcji
-.global max_diff #deklarujemy nasza funkcje jako globalna(punkt startowy programu)
+.global generate_str #deklarujemy nasza funkcje jako globalna(punkt startowy programu)
 
 	generate_str:
 	push	%rbp #zapisujemy stos
-	mov	%rsp,%rbp 
+	mov		%rsp,%rbp 
 	
-	movl	
+	movq	%rdi,%rax # przepisujemy wskaznik do stringa do rejestru ktory jest 'zwracany'
+	xchgq	%rdx,%rcx # zamieniamy miejscami %rcx i %rdx bo potrzebujemy licznik petli w %rcx
 	
+	petla:
+	movq	%rsi,(%rdi) # zapisujemy stringa z argumentu c do zarezerwowanej przestrzeni
+	incq	%rdi # przesuwamy wskaznid do zarezerwowanej przestrzeni o jedej znak
+	cmpq	$0,%rdx # sprawdzamy argument inc
+	je		inc_0 # inc == 0 ->inc_0
+	add		$1,%rsi # inc != 0 to inkrementujemy c
+	inc_0:
+	loop	petla
+
 	
+
 	mov	%rbp,%rsp 
 	pop	%rbp # przywracamy pierwotny stan stosu
 	ret	#wyjscie z funkcji
-	
 
 	
