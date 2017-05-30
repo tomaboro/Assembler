76ac7d02e985de751bffea972f4ed3123d9f2a0a
tomaboro <motek@student.agh.edu.pl>
2017-05-30 14:07:27 +0200

    first exercise done :)

--- diff.c
+++ diff.c
@@ -1,17 +1,18 @@
 //---------------------------------------------------------------
-// Program lab_6a - Asemblery Laboratorium IS II rok
+// Kolos 2015/16 CZW 11:00
 //
-// To compile&link: gcc -o lab_6a lab_6a.c lab_6a_asm.s
-// To run:          ./lab_6a
+// To compile&link: gcc -o diff diff.c diff.s
+// To run:          ./diff
 //
 //---------------------------------------------------------------
 
 #include <stdio.h>
 
-long max_diff(int a, int b, int c, int d);
+long max_diff(long a, long b, long c, long d);
 
 int main( void )
 {
+	//Running few tests
 	printf("max_diff(111,222,333,444) = %ld\n",max_diff(111,222,333,444));
 	printf("max_diff(-1,2,3,4) = %ld\n",max_diff(-1,2,3,4));
 	printf("max_diff(1,2,3,4) = %ld\n",max_diff(1,2,3,4));
--- diff.s
+++ diff.s
@@ -1,56 +1,54 @@
-.data
+#miejsce na zmienne
+.data 
 
+#miejsce na kod
 .text
-.type max_diff, @function
-.global max_diff
+.type max_diff, @function #deklaracja funkcji
+.global max_diff #deklarujemy nasza funkcje jako globalna(punkt startowy programu)
+
 	max_diff:
-	PUSH %rbp
-	MOV %rsp,%rbp
-	POP		%rax
-	POP		%rbx
-	POP		%rcx
-	POP		%rdx
+	push	%rbp #zapisujemy stos
+	mov	%rsp,%rbp 
 	
-	#CMPQ	%rax,%rbx
-	#JB		et1 #jesli a < b
-	#PUSH	%rax
-	#MOVQ	%rbx,%rax
-	#POP		%rbx
+	movq	%rdi,%rax # w rejestrze %rax zapiszemy najwieksza zmienna max = a
 	
-	#et1:
-	#CMPQ	%rax,%rcx
-	#JB		et2 #jesli a < b
-	#PUSH	%rax
-	#MOVQ	%rcx,%rax
-	#POP		%rcx
+	cmpq	%rax,%rsi 
+	jl	etykieta1 # max > b 
+	movq	%rsi,%rax # max < b -> max = b 
 	
-	#et2:
-	#CMPQ	%rax,%rdx
-	#JB		et3 #jesli a < b
-	#PUSH	%rax
-	#MOVQ	%rdx,%rax
-	#POP		%rdx
+	etykieta1:
+	cmpq	%rax,%rdx
+	jl	etykieta2 # max > c
+	movq	%rdx,%rax # max < c -> max = c 
 	
-	#et3:
-	#CMPQ	%rbx,%rcx
-	#JA		et4 #jesli b > c
-	#PUSH	%rbx
-	#MOVQ	%rcx,%rbx
-	#POP		%rcx
+	etykieta2:
+	cmpq	%rax,%rcx
+	jl	etykieta3 # max > d
+	movq	%rcx,%rax # max < d -> max = d
 	
-	#et4:
-	#CMPQ	%rbx,%rdx
-	#JB		et5 #jesli b > d
-	#PUSH	%rbx
-	#MOVQ	%rdx,%rbx
-	#POP		%rdx
+	etykieta3:
+	movq	%rdi,%rbx # w rejestrze %rbx zapiszemy najmniejsza zmienna min = a
 	
-	#et5:
-	SUB		%rbx,%rax
-	PUSH	%rdx
-	mov %rbp,%rsp
-	pop %rbp
-	ret
-	#RET
+	cmpq	%rbx,%rsi
+	jg	etykieta4 # min < b
+	movq	%rsi,%rbx # min > b -> min = b
+	
+	etykieta4:
+	cmpq	%rbx,%rdx
+	jg	etykieta5 # min < c
+	movq	%rdx,%rbx # min > c -> min = c
+	
+	etykieta5:
+	cmpq	%rbx,%rcx
+	jg	etykieta6 # min < d
+	movq	%rcx,%rbx # min > d -> min = d
+	
+	etykieta6:
+	subq	%rbx,%rax # max-min
+	
+	mov	%rbp,%rsp 
+	pop	%rbp # przywracamy pierwotny stan stosu
+	ret	#wyjscie z funkcji
+	
 
 	
